c6dd56fec0efe4674295a039253ca75b5f0bac93
Eduardo Díaz <ediaz@dcc8090.cl>
2022-10-11 00:15:27 +0000

    add initdb

---
+++ initdb.py
@@ -0,0 +1,33 @@
+import os
+import psycopg2
+
+conn = psycopg2.connect("postgres://shuzzlvk:0MjvTNPiGgx_7GbJtBs5ABA4QKPFSuS6@peanut.db.elephantsql.com/shuzzlvk")
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+# Execute a command: this creates a new table
+cur.execute('DROP TABLE IF EXISTS movies;')
+cur.execute('CREATE TABLE movies (id serial PRIMARY KEY,'
+                                 'title varchar (150) NOT NULL,'
+                                 'year INT NOT NULL,'
+                                 'director varchar (50) NOT NULL,'
+                                 'rating INT,'
+                                 'review text,'
+                                 'date_added date DEFAULT CURRENT_TIMESTAMP);'
+                                 )
+
+# Insert data into the table
+
+cur.execute('INSERT INTO movies (title, year, director, rating, review)'
+            'VALUES (%s, %s, %s, %s, %s)',
+            ('Dune', 2021, 'Dennis Villeneuve', 4, 'Great adaption of a cassic sci-fi novel'))
+cur.execute('INSERT INTO movies (title, year, director, rating, review)'
+            'VALUES (%s, %s, %s, %s, %s)',
+            ('Elvis', 2022, 'Baz Luhrmann', 4, 'Hype stylized biopic of the King'))
+
+
+conn.commit()
+
+cur.close()
+conn.close()
